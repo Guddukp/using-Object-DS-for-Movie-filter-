1fce68dd11e12c89bdf5eda7995e806317e1a83c
Guddukp <40671918+Guddukp@users.noreply.github.com>
2021-04-06 04:12:46 +0530

    Adding Filter Movies

---
+++ object/assets/scripts/objects.js
@@ -0,0 +1,62 @@
+ const addMovieBtn = document.getElementById('add-movie-btn');
+ const searchBtn  = document.getElementById('search-btn');
+ const movies = [];
+ 
+const renderMovies  = (filter = '') =>{
+    const movieList = document.getElementById('movie-list');
+
+    if(movies.length === 0){
+        movieList.classList.remove('visible');
+        return;
+    }else{
+        movieList.classList.add('visible');
+    }
+     
+    movieList.innerHTML = '';
+
+    const filterMovie = filter ? movies.filter(movie => movie.info.title.includes(filter)) : movies;
+
+    filterMovie.forEach((movie)=>{
+        const movieEl = document.createElement('li');
+        movieEl.textContent = movie.info.title;
+        let text = movie.info.title + ' - ';
+        for(const key in movie.info){
+            if(key !== 'title'){
+                text = text + `${key} : ${movie.info[key]}`;
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    });
+};
+
+const addMoiveHndler = () =>{
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+    
+    if(title.trim() === '' ||
+       extraName.trim() === ''||
+       extraValue.trim() === '' 
+    ){
+        return;
+    }
+    const newMovie = {
+         info :{
+             title,
+             [extraName]:extraValue
+         },
+         id : Math.random()
+     };
+
+     movies.push(newMovie);
+     renderMovies();
+  };
+
+  const searchFilter = ()=>{
+      const filterTerm = document.getElementById('filter-title').value;
+      renderMovies(filterTerm);
+  };
+
+  addMovieBtn.addEventListener('click',addMoiveHndler);
+  searchBtn.addEventListener('click',searchFilter);
